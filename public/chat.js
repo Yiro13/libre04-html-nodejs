6d4adb3be69eb0c3e545827102bf46a2b3cab9a2
Yahir Leo <123343914+Yiro13@users.noreply.github.com>
2023-02-21 22:30:37 -0600

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 //Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://live-super-chat.herokuapp.com');
 
 //Query DOM
 var mensaje = document.getElementById('mensaje');
